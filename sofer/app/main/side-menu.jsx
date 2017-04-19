a40edbb1d3a0a65e8c417186d3bde1ed864a7888
Michael McDermott <mmcdermott@mad-computer-scientist.com>
2017-04-19 14:50:14 -0500

    Added remaining markup and CSS from MUI example.

--- sofer/app/main/side-menu.jsx
+++ sofer/app/main/side-menu.jsx
@@ -5,7 +5,36 @@ class SideMenu extends React.Component {
     render() {
         return (
             <div id="sidedrawer" className="mui--no-user-select hide-sidedrawer">
-                Your menu options are here!
+                <div id="sidedrawer-brand" class="mui--appbar-line-height">
+                  <span class="mui--text-title">Brand.io</span>
+                </div>
+                <div class="mui-divider"></div>
+                <ul>
+                  <li>
+                    <strong>Category 1</strong>
+                    <ul>
+                      <li><a href="#">Item 1</a></li>
+                      <li><a href="#">Item 2</a></li>
+                      <li><a href="#">Item 3</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <strong>Category 2</strong>
+                    <ul>
+                      <li><a href="#">Item 1</a></li>
+                      <li><a href="#">Item 2</a></li>
+                      <li><a href="#">Item 3</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <strong>Category 3</strong>
+                    <ul>
+                      <li><a href="#">Item 1</a></li>
+                      <li><a href="#">Item 2</a></li>
+                      <li><a href="#">Item 3</a></li>
+                    </ul>
+                  </li>
+                </ul>
             </div>
         );
     }
